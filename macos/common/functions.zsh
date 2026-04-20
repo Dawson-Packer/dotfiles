115dbc806ba447ed7950dd0eff493e7d23ad70de
Dawson-Packer <125329638+Dawson-Packer@users.noreply.github.com>
2026-04-19 17:56:35 -0800

    Update functions.zsh and added web.zsh installer

--- macos/common/functions.zsh
+++ macos/common/functions.zsh
@@ -1,7 +1,3 @@
 has() {
     command -v "$1" >/dev/null 2>&1
 }
-
-clang-format-TODO() {
-    cp "$DOTFILES_DIR/clang-format/embedded-format" "./.clang-format"
-}
---
+++ macos/install/installers/web.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+print_section "Setting up Web workspace..."
+
+install_brewfile "web"
+
